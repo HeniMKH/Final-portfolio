34c66dd85957a1aca8a2b71a2900de351942aea6
Hengameh M.Khani <afsooneday@gmail.com>
2023-11-12 03:07:51 +0000

    contactbox &hover navbar

--- my-app/src/components/NavBar/navbar.js
+++ my-app/src/components/NavBar/navbar.js
@@ -2,22 +2,22 @@
 import React from 'react';
 import "./navbar.css";
 import logo from '../../assests/logo.png';
+import contactImg from '../../assests/box.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
     return (
-        <nav classroom="navbar">
+        <nav className="navbar">
             <img src="{logo}" alt="logo" className='logo' />
-            <div classroom="desktopMenu">
+            <div className="desktopMenu">
                 <Link className='destopMenuListItem'>Home</Link>
                 <Link className='destopMenuListItem'>About</Link>
                 <Link className='destopMenuListItem'>Portfolio</Link>
                 <Link className='destopMenuListItem'>Projects</Link>
             </div>
 
-            <button className='desktopMenuBtn'>
-            <img src="" alt="" className="desktopMenuBtn" />
-            </button>
+            <button className="desktopMenuBtn">
+            <img src="contactImg" alt="" className="desktopMenuImg" />Contact Me</button>
 
 
 
