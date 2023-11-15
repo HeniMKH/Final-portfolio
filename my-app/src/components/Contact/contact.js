97aa57e473286529463eabd7903c56e138fee313
Hengameh M.Khani <afsooneday@gmail.com>
2023-11-15 23:30:21 +0000

    Icons

--- my-app/src/components/Contact/contact.js
+++ my-app/src/components/Contact/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './contact.css'
+import './contact.css';
 
 function Contact() {
   return (
@@ -7,27 +7,20 @@ function Contact() {
       <div id='contact'>
         <h1 className='contactpageTitle'>Contact Me</h1>
         <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
-        <form className='contactform'></form>
-        <input type='text' className='name'placeholder='Your Name'/>
-        <input type='eamil' className='email'placeholder='Your Email'/>
-        <textarea className='msg' name='message' rows="S" placeholder='Your Message'></textarea>
-        <button type='submit'value= 'send'className='submitBtn'>Submit</button>
-        {/* <div className='link'>
-          <img src={} alt=''className='link' />
-          <img src={} alt=''className='link' />
-          <img src={} alt=''className='link' />
-        </div> */}
-           {/* <div className="hero_socials flex">
-                    <a href="https://www.linkedin.com/in/boshra-mahmoudi/" target="blank" aria-label="icons to navigate to linkedin">
-                        <AiFillLinkedin className='icon' />
-                    </a> */}
-          </div>
-
-       
-
-
+        <form className='contactform'>
+          <input type='text' className='name' placeholder='Your Name' />
+          <input type='email' className='email' placeholder='Your Email' />
+          <textarea className='msg' name='message' rows='5' placeholder='Your Message'></textarea>
+          <button type='submit' value='send' className='submitBtn'>
+            Submit
+          </button>
+        </form>
+        <div className='links'>
+          
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- my-app/src/components/Intro/intro.js
+++ my-app/src/components/Intro/intro.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './intro.css';
 import bg from '../../assests/heroImage.png';
-// import bg from '../../assests/'
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
 import { Link } from 'react-scroll'
 const Intro = () => {
   return (
@@ -11,6 +13,17 @@ const Intro = () => {
     <span className='introText'>I'm <span className='introName'>Hengameh</span> <br/> Full Stack Developer</span>
     <p className='introPara'>I am a skilled web developer with more passionate in creating Apps</p>
     <Link><button className='btn'><img src={'btnImg'} alt=''className='btnImg' />Hire Me</button></Link>
+
+    <a href="https://github.com/HeniMKH" target="blank" aria-label="icons to navigate to github">
+    <AiFillGithub className="icon" />
+    </a>
+    <a href="https://www.linkedin.com/in/hengameh-mohammadkhani-a8212417a/" target="blank" aria-label="icons to navigate to linkedin">
+    <AiFillLinkedin className='icon' />
+    </a>
+    <a href="mailto:afsooneday@gmail.com" target="_blank" aria-label="icons to navigate to email" rel="noreferrer">
+    <AiOutlineMail className='icon' />
+    </a>
+    
     </div>
 
     <img src={bg} alt="profile" className='bg'/>
